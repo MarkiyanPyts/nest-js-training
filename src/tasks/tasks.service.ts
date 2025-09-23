f53c6b9f020412a5f77765c7bda8c5a967583029
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-09-23 19:59:43 +0300

    update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,4 +4,12 @@ import { ITask } from './tasks.model';
 @Injectable()
 export class TasksService {
   private tasks: ITask[] = [];
+
+  public findAll(): ITask[] {
+    return this.tasks;
+  }
+
+  public findOne(id: string): ITask | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
 }
