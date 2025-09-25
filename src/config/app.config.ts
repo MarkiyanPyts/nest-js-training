2e4e4c95d24f85310c4e82e82978c49b31c8cc15
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-09-25 18:17:34 +0300

    improved types

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,12 @@
 import { registerAs } from '@nestjs/config';
 
-export const appConfig = registerAs('app', () => ({
-  messagePrefix: process.env.MESSAGE_PREFIX || 'Hello ',
-}));
+export interface AppConfig {
+  messagePrefix: string;
+}
+
+export const appConfig = registerAs(
+  'app',
+  (): AppConfig => ({
+    messagePrefix: process.env.MESSAGE_PREFIX || 'Hello ',
+  }),
+);
