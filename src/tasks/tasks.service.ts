0eeaf5488c6058573b33c8f8e07d482b5ae7babe
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-10-07 10:28:52 +0300

    introduce query builder

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -25,23 +25,24 @@ export class TasksService {
     filters: FindTaskParams,
     pagination: PaginationParams,
   ): Promise<[Task[], number]> {
-    const where: FindOptionsWhere<Task> = {};
+    const query = this.tasksRepository
+      .createQueryBuilder('task')
+      .leftJoinAndSelect('task.labels', 'label');
 
     if (filters.status) {
-      where.status = filters.status;
+      query.andWhere('task.status = :status', { status: filters.status });
     }
 
     if (filters.search?.trim()) {
-      where.title = Like(`%${filters.search}%`);
-      where.description = Like(`%${filters.search}%`);
+      query.andWhere(
+        '(task.title ILIKE :search OR task.description ILIKE :search)',
+        { search: `%${filters.search.trim()}%` },
+      );
     }
 
-    return await this.tasksRepository.findAndCount({
-      where,
-      relations: ['labels'], // also load all related labels,
-      skip: pagination.offset,
-      take: pagination.limit,
-    });
+    query.skip(pagination.offset).take(pagination.limit);
+
+    return query.getManyAndCount();
   }
 
   public async findOne(id: string): Promise<Task | null> {
