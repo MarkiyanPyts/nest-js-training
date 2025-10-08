41163b00fdfe912e59234129ffc44b489ad2981e
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-10-08 18:00:45 +0300

    added serialization example

--- src/users/user.entiry.ts
+++ src/users/user.entiry.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import { Task } from '../tasks/task.entiry';
 import {
   Column,
@@ -11,23 +12,29 @@ import {
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
+  @Expose()
   id: string;
 
   @Column()
+  @Expose()
   name: string;
 
   @Column()
+  @Expose()
   email: string;
 
   @Column()
   password: string;
 
   @CreateDateColumn()
+  @Expose()
   cleatedAt: Date;
 
   @UpdateDateColumn()
+  @Expose()
   upratedAt: Date;
 
   @OneToMany(() => Task, (task) => task.user)
+  @Expose()
   tasks: Task[];
 }
