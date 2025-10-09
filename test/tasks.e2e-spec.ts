3efafa23afa6c7f7bff4a0bd900d87bf27049ec6
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-10-09 18:31:08 +0300

    Update tasks.e2e-spec.ts

--- test/tasks.e2e-spec.ts
+++ test/tasks.e2e-spec.ts
@@ -2,6 +2,8 @@ import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { TestSetup } from './utils/test-setup';
 import { TaskStatus } from '../src/tasks/tasks.model';
+import { PaginationResponse } from 'src/common/pagination.response';
+import { Task } from 'src/tasks/task.entiry';
 
 describe('Authentication and Authorization (e2e)', () => {
   let testSetup: TestSetup;
@@ -58,29 +60,70 @@ describe('Authentication and Authorization (e2e)', () => {
   });
 
   it('should not allow access to other users tasks', async () => {
-    const otherUsers = {
+    const otherUser = {
       ...testUser,
       email: 'other@example.com',
     };
 
     await request(testSetup.app.getHttpServer())
       .post('/auth/register')
-      .send(otherUsers);
+      .send(otherUser);
 
     const loginResponse = await request(testSetup.app.getHttpServer())
       .post('/auth/login')
       .send({
-        email: otherUsers.email,
-        password: otherUsers.password,
+        email: otherUser.email,
+        password: otherUser.password,
       });
 
     const loginResponseBody = loginResponse.body as { accessToken: string };
 
     const otherToken = loginResponseBody.accessToken;
-    console.log(taskId);
+
     await request(testSetup.app.getHttpServer())
       .get(`/tasks/${taskId}`)
       .set('Authorization', `Bearer ${otherToken}`)
       .expect(403);
+  });
+
+  it('should list users tasks only', async () => {
+    await request(testSetup.app.getHttpServer())
+      .get(`/tasks`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .expect(200)
+      .expect((res) => {
+        const resBody = res.body as PaginationResponse<Task>;
+        console.log(resBody);
+        expect(resBody.meta.total).toBe(1);
+      });
+
+    const otherUser = {
+      ...testUser,
+      email: 'other@example.com',
+    };
+
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(otherUser);
+
+    const loginResponse = await request(testSetup.app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: otherUser.email,
+        password: otherUser.password,
+      });
+
+    const loginResponseBody = loginResponse.body as { accessToken: string };
+
+    const otherToken = loginResponseBody.accessToken;
+    await request(testSetup.app.getHttpServer())
+      .get(`/tasks`)
+      .set('Authorization', `Bearer ${otherToken}`)
+      .expect(200)
+      .expect((res) => {
+        const resBody = res.body as PaginationResponse<Task>;
+
+        expect(resBody.meta.total).toBe(0);
+      });
   });
 });
